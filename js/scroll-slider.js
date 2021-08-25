75d1ed279ff69e278d7a810c3d3b70317d0cf732
Henrique Aguiar <69041032+ApiLogik@users.noreply.github.com>
2021-08-25 16:00:33 -0300

    Add files via upload

--- js/scroll-slider.js
+++ js/scroll-slider.js
@@ -1,40 +1,42 @@
 
 function initOnScroll() {
-    const boxSlide = $('.box-sobre');
-    const boxContato = $('.box-contato');
+	const boxSlide = $('.box-sobre');
+	const boxContato = $('.box-contato');
 
-    const scrollDown = (element) => {
-        element.slideDown(1000)
-        if ($(window).width() > 1000) {
-            element.css({ 'display': 'inline-flex' });
-        }
-    }
-    
-    const scrollUp = (element) => element.slideUp(1000);
+	const scrollDown = (element) => {
+		element.slideDown(1000)
+		if ($(window).width() > 1000) {
+			element.css({ 'display': 'inline-flex' });
+		}
+	}
 
-    
-    const boxSlidePosition = boxSlide.parent().offset().top * 0.6;
-    const boxContatoPosition = boxContato.parent().offset().top * 0.96;
+	const scrollUp = (element) => element.slideUp(1000);
 
-    console.log(boxSlidePosition)
-    console.log(boxContatoPosition);
 
-    $(window).scroll(() => {
-        const scrollPosition = $(window).scrollTop();
-        console.log(scrollPosition);
-        if (scrollPosition > boxSlidePosition) {
-            scrollDown(boxSlide);
-        } else {
-            scrollUp(boxSlide);
-        }
+	const boxSlidePosition = boxSlide.parent().offset().top * 0.6;
+	const boxContatoPosition = boxContato.parent().offset().top * 0.96;
 
-        if (scrollPosition > boxContatoPosition) {
-            scrollDown(boxContato);
-        } else {
-            scrollUp(boxContato);
-        }
+	console.log(boxSlidePosition)
+	console.log(boxContatoPosition);
 
-    })
+	if (window.innerWidth > 500) {
+		$(window).scroll(() => {
+			const scrollPosition = $(window).scrollTop();
+
+			if (scrollPosition > boxSlidePosition) {
+				scrollDown(boxSlide);
+			} else {
+				scrollUp(boxSlide);
+			}
+
+			if (scrollPosition > boxContatoPosition) {
+				scrollDown(boxContato);
+			} else {
+				scrollUp(boxContato);
+			}
+		})
+	}
+
 }
 
 initOnScroll();
